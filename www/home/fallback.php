b59649e3da501be779743528594fd3694953e576
Chris <chrisdavidramos@gmail.com>
2018-05-28 23:59:14 -0400

    bump

--- www/home/fallback.php
+++ www/home/fallback.php
@@ -1,1 +1,20 @@
-This is the fallback file for home server
+<!doctype html>
+<html lang="en">
+ <head>
+  <meta charset="UTF-8"/>
+  <title>Welcome to "http-byob"!</title>
+ </head>
+ <body>
+  <header>
+
+  </header>
+  <main>
+   <a href="https://github.com/paxperscientiam/http-byob" target="_blank">project site</a>
+
+  </main>
+
+
+  <footer></footer>
+
+ </body>
+</html>
